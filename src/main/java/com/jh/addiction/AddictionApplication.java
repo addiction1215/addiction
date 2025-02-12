840e15f8037e108a2fd52f16dc88808eff7ceb60
이정현 <leejunghyun@ijeonghyeon-ui-MacBookPro.local>
2025-02-12 09:28:52 +0900

    first commit

---
+++ src/main/java/com/jh/addiction/AddictionApplication.java
@@ -0,0 +1,13 @@
+package com.jh.addiction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddictionApplication.class, args);
+    }
+
+}
