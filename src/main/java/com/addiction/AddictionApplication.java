1ee2e823c882bc6670b3d70ea377bbb5c1b9d14c
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-02 11:57:15 +0900

    feat: 공통 응답모델링

--- src/main/java/com/addiction/AddictionApplication.java
+++ src/main/java/com/addiction/AddictionApplication.java
@@ -1,9 +1,11 @@
-package com.jh.addiction;
+package com.addiction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AddictionApplication {
 
     public static void main(String[] args) {
