1d46e57efd61eb47ecbe4d9061c4c2ee7495e79b
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-02 11:59:26 +0900

    test: 테스트코드 작성 환경 구성

---
+++ src/test/java/com/addiction/IntegrationTestSupport.java
@@ -0,0 +1,34 @@
+package com.addiction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.addiction.users.entity.SettingStatus;
+import com.addiction.users.entity.SnsType;
+import com.addiction.users.entity.User;
+import com.addiction.users.feign.google.GoogleApiFeignCall;
+import com.addiction.users.feign.kakao.KakaoApiFeignCall;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+
+	@MockitoBean
+	protected KakaoApiFeignCall kakaoApiFeignCall;
+	@MockitoBean
+	protected GoogleApiFeignCall googleApiFeignCall;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	protected User createUser(String email, String password, SnsType snsType, SettingStatus settingStatus) {
+		return User.builder()
+			.email(email)
+			.password(bCryptPasswordEncoder.encode(password))
+			.snsType(snsType)
+			.settingStatus(settingStatus)
+			.build();
+	}
+}
