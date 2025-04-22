dd79ddf6ee6817cf2ca37e21e72d22ff982bd0d7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-04-22 16:26:38 +0900

    test: 설문조사 전체조회 테스트코드

--- src/test/java/com/addiction/IntegrationTestSupport.java
+++ src/test/java/com/addiction/IntegrationTestSupport.java
@@ -3,13 +3,16 @@ package com.addiction;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.addiction.global.security.SecurityService;
 import com.addiction.jwt.dto.LoginUserInfo;
+import com.addiction.survey.surveyAnswer.entity.SurveyAnswer;
+import com.addiction.survey.surveyAnswer.repository.SurveyAnswerRepository;
+import com.addiction.survey.surveyQuestion.entity.SurveyQuestion;
+import com.addiction.survey.surveyQuestion.repository.SurveyQuestionRepository;
 import com.addiction.user.push.repository.PushRepository;
 import com.addiction.user.refreshToken.repository.RefreshTokenRepository;
 import com.addiction.user.users.entity.User;
@@ -39,9 +42,15 @@ public abstract class IntegrationTestSupport {
 	protected RefreshTokenRepository refreshTokenRepository;
 	@Autowired
 	protected PushRepository pushRepository;
+	@Autowired
+	protected SurveyQuestionRepository surveyQuestionRepository;
+	@Autowired
+	protected SurveyAnswerRepository surveyAnswerRepository;
 
 	@AfterEach
 	public void tearDown() {
+		surveyAnswerRepository.deleteAllInBatch();
+		surveyQuestionRepository.deleteAllInBatch();
 		refreshTokenRepository.deleteAllInBatch();
 		pushRepository.deleteAllInBatch();
 		userRepository.deleteAllInBatch();
@@ -62,6 +71,20 @@ public abstract class IntegrationTestSupport {
 	protected LoginUserInfo createLoginUserInfo(int userId) {
 		return LoginUserInfo.builder()
 			.userId(userId)
+			.build();
+	}
+
+	protected SurveyQuestion createSurveyQuestion(String question) {
+		return SurveyQuestion.builder()
+			.question(question)
+			.build();
+	}
+
+	protected SurveyAnswer createSurveyAnswer(SurveyQuestion surveyQuestion, String answer, int score) {
+		return SurveyAnswer.builder()
+			.surveyQuestion(surveyQuestion)
+			.answer(answer)
+			.score(score)
 			.build();
 	}
 }
---
+++ src/test/java/com/addiction/survey/surveyQuestion/service/SurveyQuestionReadServiceTest.java
@@ -0,0 +1,43 @@
+package com.addiction.surveyQuestion.response;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.addiction.IntegrationTestSupport;
+import com.addiction.survey.surveyAnswer.entity.SurveyAnswer;
+import com.addiction.survey.surveyQuestion.dto.service.response.SurveyQuestionFindListServiceResponse;
+import com.addiction.survey.surveyQuestion.entity.SurveyQuestion;
+import com.addiction.survey.surveyQuestion.service.SurveyQuestionReadService;
+
+public class SurveyQuestionReadServiceTest extends IntegrationTestSupport {
+
+	@Autowired
+	private SurveyQuestionReadService surveyQuestionReadService;
+
+	@DisplayName("설문조사를 조회한다.")
+	@Test
+	void 설문조사를_조회한다() {
+		SurveyQuestion surveyQuestion = createSurveyQuestion("현재 흡연 여부를 선택해주세요");
+		surveyQuestionRepository.save(surveyQuestion);
+
+		SurveyAnswer surveyAnswer1 = createSurveyAnswer(surveyQuestion, "현재 흡연 중이며, 이제 금연하고 싶어요.", 3);
+		SurveyAnswer surveyAnswer2 = createSurveyAnswer(surveyQuestion, "현재 금연 중이며, 계속 유지하고 싶어요.", 0);
+
+		surveyAnswerRepository.save(surveyAnswer1);
+		surveyAnswerRepository.save(surveyAnswer2);
+
+		SurveyQuestionFindListServiceResponse response = surveyQuestionReadService.findAll();
+
+		assertAll(
+			() -> assertThat(response.getResponse()).extracting("id", "question")
+				.contains(Tuple.tuple(surveyQuestion.getId(), "현재 흡연 여부를 선택해주세요")),
+			() -> assertThat(response.getResponse().get(0).getSurveyAnswer()).extracting("answer")
+				.containsExactlyInAnyOrder("현재 흡연 중이며, 이제 금연하고 싶어요.", "현재 금연 중이며, 계속 유지하고 싶어요.")
+		);
+	}
+}
