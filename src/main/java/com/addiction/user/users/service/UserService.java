c06dca75274fae6a5cadb28b2338b90f7ae96439
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-04-22 18:57:33 +0900

    feat: 설문조사 결과 저장 API

--- src/main/java/com/addiction/user/users/service/UserService.java
+++ src/main/java/com/addiction/user/users/service/UserService.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.addiction.global.security.SecurityService;
+import com.addiction.survey.surveyAnswer.service.SurveyAnswerReadService;
+import com.addiction.survey.surveyResult.service.SurveyResultReadService;
 import com.addiction.user.users.dto.service.request.UserSaveServiceRequest;
 import com.addiction.user.users.dto.service.request.UserUpdateServiceRequest;
+import com.addiction.user.users.dto.service.request.UserUpdateSurveyServiceRequest;
 import com.addiction.user.users.dto.service.response.UserSaveResponse;
 import com.addiction.user.users.dto.service.response.UserUpdateResponse;
+import com.addiction.user.users.dto.service.response.UserUpdateSurveyResponse;
 import com.addiction.user.users.entity.User;
 import com.addiction.user.users.repository.UserRepository;
 
@@ -20,6 +24,8 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final SecurityService securityService;
+	private final SurveyAnswerReadService surveyAnswerReadService;
+	private final SurveyResultReadService surveyResultReadService;
 
 	public UserSaveResponse save(UserSaveServiceRequest userSaveServiceRequest) {
 		return UserSaveResponse.createResponse(userRepository.save(userSaveServiceRequest.toEntity()));
@@ -32,6 +38,19 @@ public class UserService {
 			userUpdateServiceRequest.getBirthDay()
 		);
 		return UserUpdateResponse.createResponse(user);
+	}
+
+	public UserUpdateSurveyResponse updateSurvey(UserUpdateSurveyServiceRequest userUpdateSurveyServiceRequest) {
+		User user = userRepository.findById(securityService.getCurrentLoginUserInfo().getUserId());
+		int totalScore = surveyAnswerReadService.calculateTotalScore(userUpdateSurveyServiceRequest.getAnswerId());
+
+		user.updateSurvey(
+			userUpdateSurveyServiceRequest.getPurpose(),
+			totalScore,
+			userUpdateSurveyServiceRequest.getCigarettePrice()
+		);
+
+		return UserUpdateSurveyResponse.of(user, surveyResultReadService.findClosestScore(totalScore));
 	}
 
 }
