c80dc25272da01708cdef0d0af97df919082ddfa
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-05-13 21:20:15 +0900

    feat: 회원가입도 LoginController로 이동

--- src/main/java/com/addiction/user/users/controller/LoginController.java
+++ src/main/java/com/addiction/user/users/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.addiction.user.users.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addiction.global.ApiResponse;
 import com.addiction.user.users.controller.request.LoginOauthRequest;
+import com.addiction.user.users.controller.request.UserSaveRequest;
+import com.addiction.user.users.service.UserService;
 import com.addiction.user.users.service.response.OAuthLoginResponse;
 import com.addiction.user.users.service.LoginService;
+import com.addiction.user.users.service.response.UserSaveResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import jakarta.validation.Valid;
@@ -20,10 +25,17 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 
 	private final LoginService loginService;
+	private final UserService userService;
 
 	@PostMapping("/oauth/login")
 	public ApiResponse<OAuthLoginResponse> oauthLogin(@Valid @RequestBody LoginOauthRequest loginOauthRequest) throws JsonProcessingException {
 		return ApiResponse.ok(loginService.oauthLogin(loginOauthRequest.toServiceRequest()));
+	}
+
+	@PostMapping("/join")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ApiResponse<UserSaveResponse> save(@RequestBody @Valid UserSaveRequest userSaveRequest) {
+		return ApiResponse.created(userService.save(userSaveRequest.toServiceRequest()));
 	}
 
 }
