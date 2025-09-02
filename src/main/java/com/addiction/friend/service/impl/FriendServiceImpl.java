f556fe56dccb5ef617d71fdb2321e2f8de715e68
Sangmin <sm9171@nate.com>
2025-09-02 10:33:26 +0900

    refactor: userJpaRepository -> userReadService로 변경 - userReadService에 findById 구현 메소드를 사용하기로 함

--- src/main/java/com/addiction/friend/service/impl/FriendServiceImpl.java
+++ src/main/java/com/addiction/friend/service/impl/FriendServiceImpl.java
@@ -4,17 +4,17 @@ import com.addiction.alertHistory.entity.AlertDestinationType;
 import com.addiction.firebase.FirebaseService;
 import com.addiction.firebase.request.SendFirebaseDataDto;
 import com.addiction.firebase.request.SendFirebaseServiceRequest;
-import com.addiction.friend.service.request.FriendProposalRequest;
 import com.addiction.friend.entity.Friend;
-import com.addiction.friend.entity.Friends;
 import com.addiction.friend.entity.FriendStatus;
+import com.addiction.friend.entity.Friends;
 import com.addiction.friend.repository.FriendJpaRepository;
 import com.addiction.friend.service.FriendService;
+import com.addiction.friend.service.request.FriendProposalRequest;
 import com.addiction.global.exception.AddictionException;
 import com.addiction.global.security.SecurityService;
 import com.addiction.user.push.entity.Push;
 import com.addiction.user.users.entity.User;
-import com.addiction.user.users.repository.UserJpaRepository;
+import com.addiction.user.users.service.UserReadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -29,19 +29,15 @@ import java.util.Optional;
 public class FriendServiceImpl implements FriendService {
 
     private final FriendJpaRepository friendJpaRepository;
-    private final UserJpaRepository userJpaRepository;
     private final FirebaseService firebaseService;
     private final SecurityService securityService;
+    private final UserReadService userReadService;
 
     @Override
     public void sendFriendRequest(FriendProposalRequest request) {
         Long requestId = securityService.getCurrentLoginUserInfo().getUserId();
-
-        User requester = userJpaRepository.findById(requestId)
-                .orElseThrow(() -> new AddictionException("사용자를 찾을 수 없습니다."));
-        
-        User receiver = userJpaRepository.findById(request.getReceiverId())
-                .orElseThrow(() -> new AddictionException("사용자를 찾을 수 없습니다."));
+        User requester = userReadService.findById(requestId);
+        User receiver = userReadService.findById(request.getReceiverId());
 
         if (requester.getId().equals(receiver.getId())) {
             throw new AddictionException("자기 자신에게 친구 요청을 보낼 수 없습니다.");
@@ -115,9 +111,7 @@ public class FriendServiceImpl implements FriendService {
     @Override
     public void deleteFriend(Long friendId) {
         Long currentUserId = securityService.getCurrentLoginUserInfo().getUserId();
-        
-        User currentUser = userJpaRepository.findById(currentUserId)
-                .orElseThrow(() -> new AddictionException("사용자를 찾을 수 없습니다."));
+        User currentUser = userReadService.findById(currentUserId);
         
         Friends userFriendships = friendJpaRepository.findAllAcceptedFriendshipsOfUser(currentUser);
         Friend friendship = userFriendships.findDeletableFriendBy(currentUser, friendId);
