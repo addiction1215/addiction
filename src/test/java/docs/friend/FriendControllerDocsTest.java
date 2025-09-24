de746fa5687583f0a4fbe24790c6fc0d704d3398
TNM-SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2025-09-24 13:02:06 +0900

    docs: 친구조회

--- src/test/java/docs/friend/FriendControllerDocsTest.java
+++ src/test/java/docs/friend/FriendControllerDocsTest.java
@@ -105,6 +105,8 @@ public class FriendControllerDocsTest extends RestDocsSupport {
                                         .description("응답 데이터"),
                                 fieldWithPath("data.content[]").type(JsonFieldType.ARRAY)
                                         .description("검색된 친구 프로필 리스트"),
+                                fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER)
+                                        .description("친구 PK"),
                                 fieldWithPath("data.content[].friendId").type(JsonFieldType.NUMBER)
                                         .description("친구 ID"),
                                 fieldWithPath("data.content[].nickname").type(JsonFieldType.STRING)
@@ -183,6 +185,8 @@ public class FriendControllerDocsTest extends RestDocsSupport {
                                         .description("친구 프로필 리스트"),
                                 fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER)
                                         .description("친구 UNIQUE ID"),
+                                fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER)
+                                        .description("친구 PK"),
                                 fieldWithPath("data.content[].friendId").type(JsonFieldType.NUMBER)
                                         .description("친구 ID"),
                                 fieldWithPath("data.content[].nickname").type(JsonFieldType.STRING)
