124eb293fb08d4374e99e3f83fe23a8844dbdad1
TNM-SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2025-09-30 16:01:33 +0900

    feat: Blue/Green 무중단 배포 구현

--- scripts/dev/run_new_was.sh
+++ scripts/dev/run_new_was.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app
-SCOUTER=/home/ubuntu/scouter
 
-TARGET_PORT=8080
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8080
+elif [ ${CURRENT_PORT} -eq 8080 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
