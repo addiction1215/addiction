8af0a240a8766cc1eb91ce42a0700a3e17104d15
TNM-SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2025-10-14 09:59:55 +0900

    docs: 챌린지 상세 조회 API RestDocs

--- src/test/java/docs/challenge/ChallengeControllerDocsTest.java
+++ src/test/java/docs/challenge/ChallengeControllerDocsTest.java
@@ -1,15 +1,13 @@
-package docs.challengehistory;
+package docs.challenge;
 
-import com.addiction.challenge.challengehistory.controller.ChallengeHistoryController;
-import com.addiction.challenge.challengehistory.service.ChallengeHistoryReadService;
-import com.addiction.challenge.challengehistory.service.response.ChallengeHistoryUserResponse;
+import com.addiction.challenge.challenge.controller.ChallengeController;
+import com.addiction.challenge.challenge.service.ChallengeReadService;
+import com.addiction.challenge.challenge.service.response.ChallengeDetailResponse;
 import docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -18,49 +16,46 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ChallengeHistoryControllerDocsTest extends RestDocsSupport {
+public class ChallengeControllerDocsTest extends RestDocsSupport {
 
-    private final ChallengeHistoryReadService challengeHistoryReadService = mock(ChallengeHistoryReadService.class);
+    private final ChallengeReadService challengeReadService = mock(ChallengeReadService.class);
 
     @Override
     protected Object initController() {
-        return new ChallengeHistoryController(challengeHistoryReadService);
+        return new ChallengeController(challengeReadService);
     }
 
-    @DisplayName("챌린지 이력 조회 API")
+    @DisplayName("챌린지 상세 조회 API")
     @Test
     @WithMockUser(roles = "USER")
-    void 유저의_챌린지_이력을_조회한다() throws Exception {
+    void 챌린지_상세_조회_API() throws Exception {
         // given
-        ChallengeHistoryUserResponse response1 = ChallengeHistoryUserResponse.builder()
-                .challengeId(1L)
-                .badge("badge_url_1")
-                .title("챌린지 1")
+        Long challengeId = 1L;
+        ChallengeDetailResponse response = ChallengeDetailResponse.builder()
+                .badge("badge_url")
+                .title("챌린지 제목")
+                .content("챌린지 내용")
                 .build();
 
-        ChallengeHistoryUserResponse response2 = ChallengeHistoryUserResponse.builder()
-                .challengeId(2L)
-                .badge("badge_url_2")
-                .title("챌린지 2")
-                .build();
-
-        List<ChallengeHistoryUserResponse> response = List.of(response1, response2);
-
-        given(challengeHistoryReadService.findByUserId())
-                .willReturn(response);
+        given(challengeReadService.findById(challengeId)).willReturn(response);
 
         // when // then
         mockMvc.perform(
-                        get("/api/v1/challeng-history")
+                        get("/api/v1/challenge/{challengeId}", challengeId)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("challenge-history-list",
+                .andDo(document("challenge-detail",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("challengeId").description("챌린지 ID")
+                        ),
                         responseFields(
                                 fieldWithPath("statusCode").type(JsonFieldType.NUMBER)
                                         .description("코드"),
@@ -68,14 +63,14 @@ public class ChallengeHistoryControllerDocsTest extends RestDocsSupport {
                                         .description("상태"),
                                 fieldWithPath("message").type(JsonFieldType.STRING)
                                         .description("메세지"),
-                                fieldWithPath("data[]").type(JsonFieldType.ARRAY)
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
                                         .description("응답 데이터"),
-                                fieldWithPath("data[].challengeId").type(JsonFieldType.NUMBER)
-                                        .description("챌린지 ID"),
-                                fieldWithPath("data[].badge").type(JsonFieldType.STRING)
+                                fieldWithPath("data.badge").type(JsonFieldType.STRING)
                                         .description("뱃지 URL"),
-                                fieldWithPath("data[].title").type(JsonFieldType.STRING)
-                                        .description("챌린지 제목")
+                                fieldWithPath("data.title").type(JsonFieldType.STRING)
+                                        .description("챌린지 제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING)
+                                        .description("챌린지 내용")
                         )
                 ));
     }
