9b183784abea95b0b29e9eb05329b613e8e66af7
SangkiHan <tkdrl8908@naver.com>
2026-03-10 16:17:44 +0900

    fix: 네이버 로그인 GetMapping

--- src/main/java/com/addiction/user/users/oauth/feign/naver/NaverApiFeignCall.java
+++ src/main/java/com/addiction/user/users/oauth/feign/naver/NaverApiFeignCall.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -13,7 +14,7 @@ import com.addiction.user.users.oauth.feign.naver.response.NaverUserInfoResponse
 @FeignClient(name = "${oauth.naver.api.name}", url = "${oauth.naver.api.url}", configuration = FeignClientsConfiguration.class)
 public interface NaverApiFeignCall {
 
-	@PostMapping(value = "/v1/nid/me", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@GetMapping(value = "/v1/nid/me", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	NaverUserInfoResponse getUserInfo(@RequestHeader("Authorization") String auth);
 
 }
