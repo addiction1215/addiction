6f823cd4811d410a2f6e665acfea0aca8186996b
이정현 <akdl911215@naver.com>
2026-09-23 11:14:06 +0900

    fix: return 201 when starting a craving session

--- src/main/java/com/addiction/craving/controller/CravingSessionController.java
+++ src/main/java/com/addiction/craving/controller/CravingSessionController.java
@@ -6,9 +6,11 @@ import com.addiction.craving.service.response.CravingSessionResponse;
 import com.addiction.craving.service.response.TodayCravingSummaryResponse;
 import com.addiction.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ public class CravingSessionController {
     private final CravingSessionService cravingSessionService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CravingSessionResponse> start() {
         return ApiResponse.created(cravingSessionService.start());
     }
